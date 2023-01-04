a7261489b458363a42e6617472bdbec5ac5b4a44
cintia arce <cintiaayelenarce@gmail.com>
2023-01-04 11:09:45 -0300

    Pages structure

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Ut id labore irure consequat consectetur amet qui aliqua. Ad ut non
+        pariatur deserunt. Ipsum amet incididunt cillum ea deserunt ut nostrud
+        minim elit non.
+      </p>
+    </div>
+  );
 }
 
 export default About;
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-function Index() {
-  return <div>Index</div>;
+function Events() {
+  return (
+    <div>
+      <h1>Events Page</h1>
+      <main>
+        <a href="">
+          <section>
+            <img />
+            <h2>Events in Bali</h2>
+          </section>
+        </a>
+        <a href="">
+          <section>
+            <img />
+            <h2>Events in Miami</h2>
+          </section>
+        </a>
+        <a href="">
+          <section>
+            <img />
+            <h2>Events in Tokyo</h2>
+          </section>
+        </a>
+      </main>
+    </div>
+  );
 }
 
-export default Index;
+export default Events;
