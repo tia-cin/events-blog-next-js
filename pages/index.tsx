7dde736c38054f68da4bf6e8ab4f2bc48dd7a353
cintia arce <cintiaayelenarce@gmail.com>
2023-01-04 10:56:17 -0300

    Home page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,9 +12,66 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+        <nav>
+          <img />
+          <a href="/">Home</a>
+          <a href="/events">Events</a>
+          <a href="/about">About Us</a>
+        </nav>
+      </header>
       <main>
-        <p className="font-bold">Hello </p>
+        <a href="">
+          <section>
+            <img />
+            <h2>Events in Bali</h2>
+            <p>
+              Deserunt cillum elit mollit quis nisi qui voluptate cillum
+              proident excepteur exercitation amet ad. Cillum do eu veniam
+              officia amet deserunt eiusmod ea aliquip ullamco. Veniam consequat
+              adipisicing esse elit occaecat nostrud aliquip labore.
+              Reprehenderit eu culpa minim aute aute anim labore. Dolor ipsum
+              fugiat velit consectetur ut qui laborum nostrud Lorem et. Aute
+              eiusmod occaecat tempor anim duis ullamco duis labore esse
+              cupidatat amet.
+            </p>
+          </section>
+        </a>
+        <a href="">
+          <section>
+            <img />
+            <h2>Events in Miami</h2>
+            <p>
+              Occaecat enim consequat deserunt sit exercitation amet. Irure in
+              anim laboris dolor ea quis anim irure nisi enim sint Lorem amet
+              quis. Proident consequat ea excepteur do duis excepteur anim duis.
+              Nisi officia ad pariatur sit nostrud incididunt aliquip minim
+              nulla velit nostrud elit ullamco et. Aliqua Lorem magna fugiat
+              culpa exercitation elit cupidatat proident deserunt minim dolore.
+              Sit eu excepteur elit aliqua officia duis in.
+            </p>
+          </section>
+        </a>
+        <a href="">
+          <section>
+            <img />
+            <h2>Events in Tokyo</h2>
+            <p>
+              Incididunt sunt id duis exercitation voluptate ea cillum do.
+              Laborum sint reprehenderit cupidatat id occaecat magna commodo
+              nulla. Velit eu dolore aliquip dolore non cillum magna. Ad magna
+              id dolor commodo. Culpa voluptate est culpa amet ea mollit eu est
+              eu ad duis eu duis minim.
+            </p>
+          </section>
+        </a>
       </main>
+      <footer>
+        <p>
+          Copyright © 2019 by Events Blog <br />
+          All Rights Reserved.
+        </p>
+      </footer>
     </>
   );
 }
