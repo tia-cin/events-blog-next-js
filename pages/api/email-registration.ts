1c031116f35cdb2e58e5f90dd36006548d895a4d
cintia arce <cintiaayelenarce@gmail.com>
2023-01-06 16:19:42 -0300

    email registration 1

---
+++ pages/api/email-registration.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  if (method === "POST") {
+  }
+};
+
+export default handler;
--- pages/events/[city]/[eventPage].tsx
+++ pages/events/[city]/[eventPage].tsx
@@ -3,42 +3,72 @@ import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { City, Event } from "../../../types";
 import { Title } from "../../../components";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-const EventPage: NextPage<{ event: Event }> = ({ event }) => (
-  <div>
-    <Title text={event.name.split("-").join(" ")} />
-    <section className="flex flex-col md:flex-row bg-slate-100 p-2 rounded">
-      <Image
-        src={event.image}
-        width={500}
-        height={500}
-        alt={event.name}
-        className="rounded w-full"
-      />
-      <div className="relative">
-        <p className="text-xl m-5 md:my-0 ">{event.description}</p>
-        <span className="italic absolute right-0 bottom-0">
-          - Article by Lorem Ipsum
-        </span>
-      </div>
-    </section>
-    <div className="bg-slate-100 mt-5 rounded p-2">
-      <form className="flex items-center justify-between">
-        <label>Suscribe to this Event</label>
-        <div>
-          <input
-            className="rounded p-1"
-            type="text"
-            placeholder="Enter your email"
-          />
-          <button type="submit" className="bg-blue-300 p-1 ml-1 rounded">
-            Submit
-          </button>
+const EventPage: NextPage<{ event: Event }> = ({ event }) => {
+  const router = useRouter();
+  const [email, setEmail] = React.useState<string>("");
+  const [message, setMessage] = React.useState<string>("");
+
+  const validations = () => {};
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { city, eventPage } = router.query;
+    const regex =
+      /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!email.match(regex)) {
+      setMessage("Please use a correct email address");
+    }
+
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Title text={event.name.split("-").join(" ")} />
+      <section className="flex flex-col md:flex-row bg-slate-100 p-2 rounded">
+        <Image
+          src={event.image}
+          width={500}
+          height={500}
+          alt={event.name}
+          className="rounded w-full"
+        />
+        <div className="relative">
+          <p className="text-xl m-5 md:my-0 ">{event.description}</p>
+          <span className="italic absolute right-0 bottom-0">
+            - Article by Lorem Ipsum
+          </span>
         </div>
-      </form>
+      </section>
+      <div className="bg-slate-100 mt-5 rounded p-2">
+        <form className="flex items-center justify-between" onSubmit={onSubmit}>
+          <label>Suscribe to this Event</label>
+          <div>
+            <input
+              onChange={onChange}
+              value={email}
+              className="rounded p-1"
+              type="email"
+              placeholder="Please enter your email"
+              name="email"
+            />
+            <button type="submit" className="bg-blue-300 p-1 ml-1 rounded">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default EventPage;
 
