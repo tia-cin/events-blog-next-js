d20ebb92b0ff96e5bae56459c5854af6309a27ae
cintia arce <cintiaayelenarce@gmail.com>
2023-01-06 13:00:46 -0300

    about page styled

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,51 @@
 import React from "react";
+import { Title } from "../components";
 
 function About() {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className="text-center">
+      <Title text="About Us" />
       <p>
         Ut id labore irure consequat consectetur amet qui aliqua. Ad ut non
         pariatur deserunt. Ipsum amet incididunt cillum ea deserunt ut nostrud
-        minim elit non.
+        minim elit non. Ex occaecat dolore dolore commodo do consectetur nisi
+        cupidatat. Nisi exercitation dolor aliquip sint irure amet reprehenderit
+        nulla ex voluptate ut labore nulla excepteur. Exercitation est voluptate
+        occaecat culpa et. Esse cillum culpa occaecat deserunt quis
+        reprehenderit duis elit eu ea eu. Sunt labore officia in ad proident
+        quis sint ut magna deserunt enim. Voluptate id dolor nulla exercitation.
+        Tempor mollit culpa eu Lorem ex dolore tempor ea.
       </p>
+      <section className="flex justify-center mt-10">
+        <div>
+          <Title text="Anim nostrud pariatur" />
+          <p>
+            Ex occaecat sint cupidatat quis ut tempor ad in est et nostrud. In
+            eiusmod ut in occaecat enim elit consequat commodo ut incididunt
+            adipisicing ea proident. Incididunt ad nisi nostrud excepteur non
+            ipsum culpa.Mollit pariatur tempor velit incididunt magna enim
+            voluptate. Nostrud ullamco magna qui est occaecat ullamco laborum.
+            Non eiusmod eiusmod ea nulla labore excepteur magna quis labore quis
+            deserunt est pariatur. Amet cupidatat enim magna in commodo laborum.
+            Aute pariatur Lorem amet non non exercitation laborum eu nulla
+            consectetur dolore non elit non.
+          </p>
+        </div>
+        <div>
+          <Title text="Aliquip ipsum anim officia" />
+          <p>
+            Fugiat sit cillum Lorem ullamco. Aliquip deserunt id exercitation
+            quis fugiat voluptate tempor esse. Anim cillum amet cillum anim
+            officia reprehenderit duis laborum officia anim anim amet et labore.
+            Excepteur eiusmod reprehenderit commodo aliquip do cupidatat ex
+            eiusmod tempor cillum ea irure qui et. Non amet minim officia
+            consectetur duis eiusmod veniam irure. Ullamco elit do ad velit
+            irure deserunt laboris anim non officia eiusmod dolor amet. Sint
+            aliqua amet qui cupidatat ut eiusmod magna ea exercitation officia
+            enim proident anim anim.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
