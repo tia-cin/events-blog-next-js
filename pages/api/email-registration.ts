6c0aff8b5a4c0c33030689db96c7cb5c04b12774
cintia arce <cintiaayelenarce@gmail.com>
2023-01-06 18:25:15 -0300

    add comments

--- pages/api/email-registration.ts
+++ pages/api/email-registration.ts
@@ -3,6 +3,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
   if (method === "POST") {
+    const { email, eventPage, city } = req.body;
+
+    res.status(200).json({ message: "Correct" });
   }
 };
 
--- pages/events/[city]/[eventPage].tsx
+++ pages/events/[city]/[eventPage].tsx
@@ -11,29 +11,39 @@ const EventPage: NextPage<{ event: Event }> = ({ event }) => {
   const [email, setEmail] = React.useState<string>("");
   const [message, setMessage] = React.useState<string>("");
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  //updates "email" State value
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
+  // send data to API
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const { city, eventPage } = router.query;
+    // validation regular expression
     const regex =
       /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+    // if there is no match, correct value format
     if (!email.match(regex)) {
       setMessage("Please use a correct email address");
     }
 
     try {
+      // send to API the data
       const response = await axios.post(
-        "/api/email-registration",
-        { email, city, eventPage },
+        "/api/email-registration", // URL
+        { email, city, eventPage }, // DATA
         { headers: { "Content-Type": "application/json" } }
       );
 
+      // if response failed, send an Error
       if (response.status !== 200)
         throw new Error(`Status Code: ${response.status}`);
 
+      // send "correct" message
       setMessage(response.data.message);
+      // reset "email" value
       setEmail("");
     } catch (error) {
       console.log(error);
@@ -68,7 +78,6 @@ const EventPage: NextPage<{ event: Event }> = ({ event }) => {
               className="rounded p-1"
               type="email"
               placeholder="Please enter your email"
-              name="email"
             />
             <button type="submit" className="bg-blue-300 p-1 ml-1 rounded">
               Submit
@@ -86,17 +95,21 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const { events } = await require("../../../data/data.json");
 
   const paths = events
-    .map((c: City) => {
-      return c.events.map((ev: Event) => {
-        return {
-          params: {
-            city: c.city,
-            eventPage: ev.name.toString(),
-          },
-        };
-      });
-    })
-    .flat(1);
+    .map(
+      (
+        c: City // new array (lenght: events)
+      ) =>
+        c.events.map((ev: Event) => {
+          // new array (lenght: city events)
+          return {
+            params: {
+              city: c.city, // each city
+              eventPage: ev.name.toString(), // each event of city
+            },
+          };
+        })
+    )
+    .flat(1); // concat city arry with events array
 
   return {
     paths,
@@ -107,6 +120,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { events } = await require("../../../data/data.json");
   const { city, eventPage }: any = params;
+  // get "eventPage" from "city"
   const event = events
     .find((c: City) => c.city === city)
     .events.find((ev: Event) => ev.name === eventPage);
