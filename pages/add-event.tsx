9894632861525812189f86df63d6c3f7db14cf7f
cintia arce <cintiaayelenarce@gmail.com>
2023-01-08 17:54:45 -0300

    add cities

--- pages/add-event.tsx
+++ pages/add-event.tsx
@@ -1,8 +1,9 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import React from "react";
 import { Title } from "../components";
+import { City } from "../types";
 
-const AddEvent: NextPage = () => {
+const AddEvent: NextPage<{ cities: any }> = ({ cities }) => {
   const [inputs, setInputs] = React.useState({
     name: "",
     city: "",
@@ -11,7 +12,9 @@ const AddEvent: NextPage = () => {
   });
 
   const onChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
   ) => {
     setInputs({
       ...inputs,
@@ -38,14 +41,14 @@ const AddEvent: NextPage = () => {
           </div>
           <div className="flex flex-col justify-center">
             <label className="text-lg font-semibold">Event City</label>
-
-            <input
-              type="text"
-              name="city"
-              placeholder="Event City"
-              className="rounded p-1"
-              onChange={onChange}
-            />
+            <select name="city" onChange={onChange} className="rounded p-1">
+              <option className="capitalize">Add New City</option>
+              {cities.map((city: string, i: number) => (
+                <option className="capitalize" key={i}>
+                  {city}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="flex flex-col justify-center">
             <label className="text-lg font-semibold">Event Description</label>
@@ -72,3 +75,14 @@ const AddEvent: NextPage = () => {
 };
 
 export default AddEvent;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { events } = await require("../data/data.json");
+  const cities = events.map((city: City) => city.city);
+
+  return {
+    props: {
+      cities,
+    },
+  };
+};
