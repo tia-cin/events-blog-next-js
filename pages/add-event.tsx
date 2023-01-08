78cf8809e3a8395a74b1909f54d4802e1a7923be
cintia arce <cintiaayelenarce@gmail.com>
2023-01-08 17:42:22 -0300

    add event page structure & style

--- pages/add-event.tsx
+++ pages/add-event.tsx
@@ -1,28 +1,47 @@
+import { NextPage } from "next";
 import React from "react";
 import { Title } from "../components";
 
-function AddEvent() {
+const AddEvent: NextPage = () => {
   return (
     <div>
       <Title text="Add New Event" />
       <div>
-        <form>
-          <div>
-            <input type="text" placeholder="Event Name" />
+        <form className="grid grid-cols-2 gap-2">
+          <div className="flex flex-col justify-center">
+            <label className="text-lg font-semibold">Event Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Event Name"
+              className="rounded p-1"
+            />
           </div>
-          <div>
-            <input type="text" placeholder="Event City" />
+          <div className="flex flex-col justify-center">
+            <label className="text-lg font-semibold">Event City</label>
+            <input
+              type="text"
+              name="city"
+              placeholder="Event City"
+              className="rounded p-1"
+            />
           </div>
-          <div>
-            <textarea placeholder="Event Description" />
+          <div className="flex flex-col justify-center">
+            <label className="text-lg font-semibold">Event Description</label>
+            <textarea
+              name="description"
+              placeholder="Event Description"
+              className="w-700 h-200 rounded p-1"
+            />
           </div>
-          <div>
-            <input type="image" placeholder="Event Picture" />
+          <div className="flex flex-col justify-center">
+            <label className="text-lg font-semibold">Event Picture</label>
+            <input type="image" className="w-700 h-200" name="image" />
           </div>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default AddEvent;
