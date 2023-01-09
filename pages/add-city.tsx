856b115945ef57518cce4bae0d9e6b9651fccc78
cintia arce <cintiaayelenarce@gmail.com>
2023-01-09 14:38:22 -0300

    add new city page stucture

--- pages/add-city.tsx
+++ pages/add-city.tsx
@@ -6,8 +6,31 @@ const AddCity: NextPage = () => {
   return (
     <div>
       <Title text="Add New City" />
-      <div>
-        <form></form>
+      <div className="mt-5">
+        <form>
+          <div className="flex flex-col justify-center">
+            <label className="text-lg font-semibold">City Name</label>
+            <input
+              type="text"
+              placeholder="City Name"
+              className=" rounded p-1"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-5 mt-10">
+            <div className="flex flex-col justify-center">
+              <label className="text-lg font-semibold">Event Description</label>
+              <textarea
+                name="description"
+                placeholder="City Description"
+                className="w-full h-200 rounded p-1"
+              />
+            </div>
+            <div className="flex flex-col justify-center">
+              <label className="text-lg font-semibold">City Picture</label>
+              <input type="image" className="w-full h-200" name="image" />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
