014f5810968eb7be07c167c82c943f157ff2a02c
cintia arce <cintiaayelenarce@gmail.com>
2023-01-10 19:05:37 -0300

    add event func

--- pages/api/add-event.ts
+++ pages/api/add-event.ts
@@ -1,3 +1,46 @@
-const handler = () => {};
+import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import { cwd } from "process";
+import path from "path";
+import { City } from "../../types";
+
+// create path from project base path to the data file
+const buidPath = () => path.join(cwd(), "data", "data.json");
+
+// get content of the file and return it as object
+const extractData = (file: string) => {
+  const data = fs.readFileSync(file);
+  return JSON.parse(data.toString("utf8"));
+};
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  // get location of data JSON
+  const filePath = buidPath();
+  // get data from prev constant
+  const { events } = extractData(filePath);
+
+  // data not found, send message
+  if (!events) return res.status(404).json({ message: "Events not found" });
+
+  if (method === "POST") {
+    const { name, description, image, city } = req.body;
+
+    // if user input format is not correct, send message to fix it
+    if (!name || !description || !image) {
+      res.status(422).json({ message: "Please submit data requested" });
+    }
+
+    // create a copy of data
+    let newEvents: City[] = events;
+
+    fs.writeFileSync(filePath, JSON.stringify({ events: newEvents }));
+
+    res.status(200).json({
+      message: `Event ${name} in ${city} has been added to our events`,
+    });
+  }
+};
 
 export default handler;
